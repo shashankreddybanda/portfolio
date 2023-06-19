75bc0fbd9cf56a435de4c274dd1e93bebbfe6c2a
shashankreddybanda <shashank24140544@gmail.com>
2023-06-19 15:35:49 -0700

    modified astro config to correct url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/shashankreddybanda',
+	site: 'https://shashankreddybanda.github.io',
 	base: '/portfolio',
 });
