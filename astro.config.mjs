62ac1a1b8ef952836b6ab9245d39a166de03009e
shashankreddybanda <shashank24140544@gmail.com>
2024-03-10 17:22:44 -0700

    "addedGoogleAnalytics"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://shashankreddybanda.github.io',
 	base: '/portfolio',
+	integrations: [
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		})
+	]
 });
